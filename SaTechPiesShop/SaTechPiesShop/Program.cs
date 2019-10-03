be15e9cf49b20717d949297ee3ab93b2cdf5d950
Mu7ammadSaad <engmohamedsaad2011@gmail.com>
2019-10-03 09:34:53 +0300

    Adding EF Core, Initialize the database, making migrations

--- SaTechPiesShop/SaTechPiesShop/Program.cs
+++ SaTechPiesShop/SaTechPiesShop/Program.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SaTechPiesShop.Models;
 
 namespace SaTechPiesShop
 {
@@ -14,9 +16,25 @@ namespace SaTechPiesShop
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+            var host = CreateWebHostBuilder(args).Build();
 
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<AppDbContext>();
+                    DbInitializer.Seed(context);
+                }
+                catch (Exception)
+                {
+                    //we could log this in a real-world situation
+                }
+            }
+
+            host.Run();
+
+        }
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
